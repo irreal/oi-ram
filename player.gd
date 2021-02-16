ba2ea8a563953dea8dabea4d0af9366b05c82d1f
Milos Spasojevic <milos.s.pfc@gmail.com>
2021-02-16 23:57:36 +0100

    Initial commit

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var hSpeed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if abs(hSpeed) > 0:
+		$AnimatedSprite.animation = 'run'
+	if hSpeed < 0:
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.flip_h = false
+		
